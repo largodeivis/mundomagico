ee2b9ff123223ae86ce9188448df9c2456f7d32c
Deivis Largo <dlargo67@gmail.com>
2018-09-26 22:49:23 -0500

    First commit

---
+++ mundomagico/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<html>
+<title>
+Mundo Magico
+</title>
+<body>
+Welcome to Mundo Magico Home Care Center.
+</body>
+</html>
+
---
+++ mundomagico/src/test/java/com/mundomagico/mundomagico/MundomagicoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mundomagico.mundomagico;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MundomagicoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
